db3f002e9bb793c483b34607bda774da3de6d2ef
matviy <matviy13@proton.me>
2026-03-25 17:28:46 +0200

    theme changes + telescope path display

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,15 +21,9 @@ return {
     },
     config = function()
       require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          path_display = { 'smart' },
+        },
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
         },
